0dd4f160c36048cbd27140603666e6bac7200219
aln-f <144757715+aln-f@users.noreply.github.com>
2026-06-04 16:24:21 -0400

    Add dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,12 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    json = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json=json, headers=headers, timeout=1.5)
-    return response.text
+    payload = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json=payload, headers=headers, timeout=1.5)
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    emotions['dominant_emotion'] = sorted(emotions, key=emotions.get)[-1]
+    return emotions
