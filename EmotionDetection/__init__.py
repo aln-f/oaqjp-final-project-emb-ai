1b9d303a98c52f8d34d586a84605a476dccc649e
aln-f <144757715+aln-f@users.noreply.github.com>
2026-06-30 21:59:56 -0400

    Format code according to PEP8

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,1 +1,3 @@
+''' This package performs emotion detection on a provided text.
+'''
 from . import emotion_detection
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+'''Flask application for Emotion Detection.'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +6,10 @@ app = Flask('Emotion Detector')
 
 @app.route('/emotionDetector')
 def emot_detector():
+    '''Run emotion detection on text (received from the HTML interface)
+    and return 5 core emotions, their confidence scores, and the
+    dominant emotion.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     res = emotion_detector(text_to_analyze)
     if res['dominant_emotion'] is None:
@@ -13,12 +18,14 @@ def emot_detector():
         "For the given statement, the system response is " + 
         f"'anger': {res['anger']}, 'disgust': {res['disgust']}, " +
         f"'fear': {res['fear']}, 'joy': {res['joy']} and " +
-        f"'sadness': {res['sadness']}. The dominant emotion is {res['dominant_emotion']}."
+        f"'sadness': {res['sadness']}. " +
+        f"The dominant emotion is {res['dominant_emotion']}."
     )
     return text
 
 @app.route('/')
 def index():
+    '''Render the main application page.'''
     return render_template('index.html')
 
 if __name__ == '__main__':
